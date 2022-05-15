1440d93f78a185a7f19cff100b1898029bb4cc11
jadonhs185 <h.s.jadon185@gmail.com>
2022-05-15 22:05:09 +0530

    adapter design pattern implemented in conceptual example

--- structural/Adapter_conceptual.cs
+++ structural/Adapter_conceptual.cs
@@ -2,7 +2,11 @@ using System;
 
 namespace DesignPatterns_CSharp.structural
 {
-    public class ConcreteTarget
+    public interface ITarget
+    {
+        public void GetRequest();
+    }
+    public class ConcreteTarget : ITarget
     {
         public void GetRequest()
         {
@@ -13,17 +17,37 @@ namespace DesignPatterns_CSharp.structural
     //is not practical to change this
     public class Adaptee
     {
-        public void GetSpecificRequest()
+        public string GetSpecificRequest()
         {
-            Console.WriteLine("specific request");
+            return "specific request";
+        }
+    }
+    //This adapter wraps the adaptee object and provides it with a new interface 
+    //so existing client classes can use this object even though the original inteface is incompatible
+    public class Adapter : ITarget
+    {
+        private Adaptee adaptee;
+        public Adapter(Adaptee adaptee)
+        {
+            this.adaptee = adaptee;
+        }
+        public void GetRequest()
+        {
+            Console.WriteLine($"This is {this.adaptee.GetSpecificRequest()}");
         }
     }
     public class Client_adapter_conc
     {
         public void ClientCode()
         {
-            ConcreteTarget concreteTargetObj = new ConcreteTarget();
+            //The client is calling the specific target obj
+            //
+            ITarget concreteTargetObj = new ConcreteTarget();
             concreteTargetObj.GetRequest();
+
+            Adaptee adaptee = new Adaptee();
+            ITarget adapter = new Adapter(adaptee);
+            adapter.GetRequest();
         }
     }
 }
